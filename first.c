06c0ad3bd76c17fe6aef9370d440f398c365efd8
Rati Raj <ratiraj3011@gmail.com>
2025-10-07 15:22:02 +0530

    Create first.c

---
+++ first.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+        int x=1;
+        int exp= 2< !0 +7/10*3;
+        if(exp) {
+                printf("Yes");
+        }
+        else {
+                printf("No");
+        }
+        return 0;
+}
